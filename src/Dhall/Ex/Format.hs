40889b95eb9ee666799aefd3341fd5a920ee5a0b
MATSUBARA Nobutada <nobutada.matsubara@mixi.co.jp>
2018-11-19 00:03:38 +0900

    Feat: pretty format

--- src/Dhall/Ex/Format.hs
+++ src/Dhall/Ex/Format.hs
@@ -8,43 +8,60 @@ module Dhall.Ex.Format where
 
 import           RIO
 
-import           Data.HashMap.Strict.InsOrd (InsOrdHashMap)
-import qualified Data.HashMap.Strict.InsOrd as InsOrdHashMap
-import           Dhall.Core                 (Expr (..))
-import qualified Dhall.Core                 as Dhall
-import           Dhall.Ex.Env               (Env)
-import qualified Dhall.Import               as Dhall
-import qualified Dhall.Parser               as Dhall
+import           Data.HashMap.Strict.InsOrd            (InsOrdHashMap)
+import qualified Data.HashMap.Strict.InsOrd            as InsOrdHashMap
+import qualified Data.Text.Prettyprint.Doc             as Pretty
+import qualified Data.Text.Prettyprint.Doc.Render.Text as Pretty
+import           Dhall.Core                            (Expr (..))
+import qualified Dhall.Core                            as Dhall
+import           Dhall.Ex.Env                          (Env)
+import qualified Dhall.Import                          as Dhall
+import qualified Dhall.Parser                          as Dhall
 
 run :: FilePath -> RIO Env ()
 run path = do
   logDebug $ fromString ("read file: " <> path)
   txt <- readFileUtf8 path
-  case Dhall.exprFromText path txt of
-    Left e  -> logError $ displayShow e
-    Right e -> do
-      e' <- liftIO $ Dhall.load e
-      logDebug $ display ("expr: " <> tshow e')
-      case sortFieldsByType e' of
-        Left key  -> logError $ display ("not found key: " <> key)
-        Right e'' -> logInfo $ display (Dhall.pretty e'')
+  case Dhall.exprAndHeaderFromText path txt of
+    Left e               -> logError $ displayShow e
+    Right (header, expr) -> do
+      expr' <- liftIO $ Dhall.load expr
+      logDebug $ display ("expr: " <> tshow expr')
+      case Dhall.denote expr' of
+        (Annot _ t) -> writeWithPrettyAndSort path (header, expr) t
+        _           -> writeWithPretty path (header, expr)
 
-sortFieldsByType :: Expr s a -> Either Text (Expr s a)
-sortFieldsByType = \case
-  (Note s e)  -> Note s <$> sortFieldsByType e
-  (Annot e t) -> Annot <$> (e `sortFieldsBy` Dhall.denote t) <*> pure t
-  e           -> pure $ e
+writeWithPrettyAndSort :: (Show s, Show a, Pretty.Pretty a)
+  => FilePath -> (Text, Expr s a) -> Expr s b -> RIO Env ()
+writeWithPrettyAndSort path (header, expr) t =
+  case expr `sortFieldsBy` Dhall.denote t of
+    Left key    -> logError $ display ("not found key: " <> key)
+    Right expr' -> writeWithPretty path (header, expr')
 
-sortFieldsBy :: Expr s a -> Expr s a -> Either Text (Expr s a)
+sortFieldsBy :: Expr s a -> Expr s b -> Either Text (Expr s a)
 sortFieldsBy e t = case (e, t) of
   (Note s e', _)               -> Note s <$> (e' `sortFieldsBy` t)
+  (Annot e' t', _)             -> Annot <$> (e' `sortFieldsBy` t) <*> pure t'
   (RecordLit r, Record rt)     -> RecordLit <$> (r `sortKeyBy` rt)
   (ListLit lt es, App List t') -> ListLit lt <$> traverse (`sortFieldsBy` t') es
   _                            -> pure $ e
 
 sortKeyBy
   :: InsOrdHashMap Text (Expr s a)
-  -> InsOrdHashMap Text (Expr s a)
+  -> InsOrdHashMap Text (Expr s b)
   -> Either Text (InsOrdHashMap Text (Expr s a))
 sortKeyBy r = InsOrdHashMap.traverseWithKey $ \k t ->
   (maybe (Left k) pure $ InsOrdHashMap.lookup k r) >>= (`sortFieldsBy` t)
+
+writeWithPretty :: (Show s, Show a, Pretty.Pretty a)
+  => FilePath -> (Text, Expr s a) -> RIO Env ()
+writeWithPretty path (header, expr) = do
+  logDebug $ display ("write expr: " <> tshow expr)
+  let doc = Pretty.pretty header <> Pretty.pretty expr
+  writeFileUtf8 path $ Pretty.renderStrict (Pretty.layoutSmart opts doc)
+
+
+opts :: Pretty.LayoutOptions
+opts =
+  Pretty.defaultLayoutOptions
+    { Pretty.layoutPageWidth = Pretty.AvailablePerLine 80 1.0 }
